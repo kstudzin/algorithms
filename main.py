80a5def5ec86ebf2e4f023ba057d07e93c481663
Kate Studzinski <kstudzinski@acm.org>
2022-05-20 13:08:47 -0400

    Experiment with generationg connected graphs
    Using networkx library, expertiment with generating connected graphs that are not complete.

--- main.py
+++ main.py
@@ -16,6 +16,18 @@ def main():
         plt.show()
         print(nx.is_connected(graph))
 
+        # for graph with e edges, k can be in the range 1 to e - 1
+        # if k is e - 1, the graph will be fully connected. If edges
+        # are unweighted, shortest path computation is trivial
+        # if k is 1, graph will be simple and shortest path is straight forward
+        aug = nx.k_edge_augmentation(graph, 5)
+        graph.add_edges_from(aug)
+        subax1 = plt.subplot(121)
+        nx.draw(graph, with_labels=True, font_weight='bold')
+        plt.show()
+        print(nx.is_connected(graph))
+
+
 
 if __name__ == '__main__':
     main()
