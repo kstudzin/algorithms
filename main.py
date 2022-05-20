97a9e416c255ab5a73ee489843196552b8da6d97
Kate Studzinski <kstudzinski@acm.org>
2022-05-20 12:50:27 -0400

    Test networkx graph generation

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-import random
+import networkx as nx
+import matplotlib.pyplot as plt
+
+frontier = []
 
 
-def generate_points(num_points):
-    return {(random.randrange(num_points), random.randrange(num_points)) for i in range(num_points)}
+def generate_graph(num_v, num_e):
+    return nx.gnm_random_graph(num_v, num_e)
 
 
 def main():
-    points = generate_points(5)
-    print(points)
+    for i in range(10):
+        graph = generate_graph(10, 5)
+        subax1 = plt.subplot(121)
+        nx.draw(graph, with_labels=True, font_weight='bold')
+        plt.show()
+        print(nx.is_connected(graph))
 
 
 if __name__ == '__main__':
